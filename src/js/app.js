ccec3a87a9c94c5e50a7bfebb8eafdd75ab0d60d
fjaviermartinezalonso <54067143+fjaviermartinezalonso@users.noreply.github.com>
2024-05-17 21:03:41 +0200

    Add scroll smooth inside page

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     navegacionFija();
     crearGaleria();
     resaltarEnlace();
+    scrollNav();
 });
 
 function navegacionFija() {
@@ -9,7 +10,7 @@ function navegacionFija() {
     const festival = document.querySelector(".sobre-festival");
 
     window.addEventListener("scroll", () => {
-        if(festival.getBoundingClientRect().bottom < 1) { // en cuanto lo pasemos fijamos barra
+        if (festival.getBoundingClientRect().bottom < 1) { // en cuanto lo pasemos fijamos barra
             header.classList.add("fixed");
         }
         else {
@@ -21,8 +22,8 @@ function navegacionFija() {
 function crearGaleria() {
     const galeria = document.querySelector(".galeria-imagenes");
     const NUM_IMAGENES = 16;
-    
-    for(let i=1; i<=NUM_IMAGENES; i++) {
+
+    for (let i = 1; i <= NUM_IMAGENES; i++) {
         const imagen = document.createElement("IMG");
         imagen.src = `src/img/gallery/full/${i}.jpg`;
         imagen.alt = "Imagen galería";
@@ -35,7 +36,7 @@ function mostrarImagen(i) {
     const imagen = document.createElement("IMG");
     imagen.src = `src/img/gallery/full/${i}.jpg`;
     imagen.alt = "Imagen galería";
-    
+
     // const cerrarBtn = document.createElement("BUTTON");
     // cerrarBtn.textContent = "X";
     // cerrarBtn.classList.add("btn-cerrar");
@@ -55,7 +56,7 @@ function mostrarImagen(i) {
 function cerrarModal() {
     const body = document.querySelector("body");
     body.classList.remove("overflow-hidden"); // permitimos scroll
-    
+
     const modal = document.querySelector(".modal");
     modal.classList.add("fade-out");
     setTimeout(() => {
@@ -69,21 +70,33 @@ function resaltarEnlace() {
         const navLinks = document.querySelectorAll(".navegacion-principal a");
 
         let sectionActualId = "";
-        sections.forEach( section => {
+        sections.forEach(section => {
             const sectionTop = section.offsetTop; // distancia con el top del body
             const sectionHeight = section.clientHeight; // tamaño del section
-            if(window.scrollY >= (sectionTop - sectionHeight/3)) {
+            if (window.scrollY >= (sectionTop - sectionHeight / 3)) {
                 sectionActualId = section.id; // si he sobrepasado la mayor parte de la section
             }
         });
 
-        navLinks.forEach( link => {
-            if(link.getAttribute("href") === `#${sectionActualId}`) {
+        navLinks.forEach(link => {
+            if (link.getAttribute("href") === `#${sectionActualId}`) {
                 link.classList.add("active");
             }
             else {
                 link.classList.remove("active");
             }
+        });
+    });
+}
+
+function scrollNav() {
+    const navLinks = document.querySelectorAll(".navegacion-principal a");
+    navLinks.forEach(link => {
+        link.addEventListener("click", e => {
+            e.preventDefault();
+            const sectionScroll = e.target.getAttribute("href");
+            const section = document.querySelector(sectionScroll);
+            section.scrollIntoView({behavior: "smooth"});
         });
     });
 }
